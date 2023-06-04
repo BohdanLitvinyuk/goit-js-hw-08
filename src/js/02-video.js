72ad90c1cfe9c2ec0de4dad56295c1c878d1019e
Bohdan Lytvyniuk <lytvyniukbohdan@gmail.com>
2023-06-04 18:49:29 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+import Vimeo from '@vimeo/player';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const saveData = localStorage.getItem('videoplayer-current-time');
+const parsedData = JSON.parse(saveData);
+
+player.on('timeupdate', throttle(function (event) {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+}, 1000));
+
+
+player.setCurrentTime(parsedData).then(function(seconds) {
+    // seconds = the actual time that the player seeked 
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+        
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+   

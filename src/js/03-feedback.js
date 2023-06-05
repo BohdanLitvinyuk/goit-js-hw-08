f5487ae733355e3b78ee52eaa730bcca94a00e95
Bohdan Lytvyniuk <lytvyniukbohdan@gmail.com>
2023-06-05 20:58:45 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from "lodash.throttle";
 
+
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle((onFormInput),500));
+
+const keyLogin = 'feedback-form-state';
+const data = JSON.parse(localStorage.getItem(keyLogin));
+
+    if (JSON.parse(localStorage.getItem(keyLogin))) {
+        form.elements.email.value = data.email;
+        form.elements.message.value = data.message;
+    }
+
+function onFormInput() {
+    const userData = {
+      email:form.elements.email.value,
+    message:form.elements.message.value
+  }
+    localStorage.setItem(keyLogin, JSON.stringify(userData));
+};
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    if (JSON.parse(localStorage.getItem(keyLogin))) {
+        localStorage.removeItem(keyLogin);
+        form.reset();
+        return;
+    }
+    
+};
